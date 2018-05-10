8a2c2652b8d5353c9ddb1b836ad3889ea331011c
arionitmisini <arionitmisini@gmail.com>
2018-05-11 00:23:47 +0200

    Updated: Form of Registe/Login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -5,7 +5,26 @@ class Login extends Component {
   render() {
     return (
       <div>
-        <h1>Login</h1>
+        <div className="login">
+            <div className="container">
+            <div className="row">
+                <div className="col-md-8 m-auto">
+                <h1 className="display-4 text-center">Log In</h1>
+                <p className="lead text-center">Sign in to your PolyDo account</p>
+                <form action="dashboard.html">
+                    <div className="form-group">
+                    <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" />
+                    </div>
+                    <div className="form-group">
+                    <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" />
+                    </div>
+                    <input type="submit" className="btn btn-info btn-block mt-4" />
+                </form>
+                </div>
+            </div>
+            </div>
+        </div>
+
       </div>
     );
   }
