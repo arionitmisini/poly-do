68805afd95d8ff4aa2c6cfafdafb8a9ea6acb0ad
arionitmisini <arionitmisini@gmail.com>
2018-05-10 23:27:44 +0200

    Updated: Register and Login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
+import {Link} from "react-router-dom"
 
 class Login extends Component {
   render() {
     return (
       <div>
-        
+        <h1>Login</h1>
       </div>
-    )
+    );
   }
 }
 
