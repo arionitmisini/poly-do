56276f240a35c1491bc0105044e22d90712f1091
arionitmisini <arionitmisini@gmail.com>
2018-05-10 23:07:03 +0200

    Features: Added auth pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom"; 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
@@ -7,11 +8,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
       <Navbar/>
+      <Route exact path="/" component = {Landing}/>
       <Landing/>
       <Footer/>
       </div>
+      </Router>
     );
   }
 }
