99db241c66f81ff3796ce516263b30bf8c76ee2a
arionitmisini <arionitmisini@gmail.com>
2018-05-11 19:32:47 +0200

    Sample mocha test

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,7 +1,11 @@
-const utils = require('.../api/profile');
+const utils = require('./utils');
 
-it('Shpuld show profile for this user', () => {
-  var utils = utils.Error == 404;
+it('should add two numbers', () => {
+  var res = utils.add(33, 11);
+
+  if (res !== 44) {
+    throw new Error(`Expected 44, but got ${res}.`)
+  }
 });
 
 it('should square a number', () => {
